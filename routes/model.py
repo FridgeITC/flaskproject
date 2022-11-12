60d2742b7cd86108101ff0392ac1a40602d7bfc1
Mau-MR <maudevops@gmail.com>
2022-11-12 15:48:51 -0600

    Receiving the image to make the inference

--- routes/model.py
+++ routes/model.py
@@ -1,7 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
 model = Blueprint('model', __name__)
 
-@model.get('/')
-def get_all():
-  return []
+
+@model.route('/image', methods=['POST'])
+def inference():
+    if 'image' not in request.files:
+        return jsonify({"done": False, 'message': 'There was no image for inference'})
+    image = request.files['image']
+    print(image)
+    return []
