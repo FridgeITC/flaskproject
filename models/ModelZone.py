e135867ac15a14045ecf878423fd41d240da6baa
Alejandro Hernández <werunthenite@gmail.com>
2022-11-13 17:20:55 -0600

    Zone and local models implemented

---
+++ models/ModelZone.py
@@ -0,0 +1,73 @@
+from flask import jsonify, request
+from db import mysql
+
+class ModelZone():
+    @classmethod
+    def get_all(self):
+        db = None
+        cursor = None
+        try:
+          db = mysql.connect()
+          if request.method == 'GET':
+            sql = "SELECT * FROM zone"
+            cursor = db.cursor()
+            cursor.execute(sql)
+            data = cursor.fetchall()
+            print(data)
+            db.commit()
+            resp = jsonify(data)
+            resp.status_code = 200
+            return resp
+        except Exception as e:
+          print(e)
+        finally:
+          cursor.close() 
+          db.close()
+
+    @classmethod
+    def add(self, _json):
+        db = None
+        cursor = None
+        try:
+          db = mysql.connect()
+          _json = request.json
+          _name = _json['name']
+
+          if _name and request.method == 'POST':
+            sql = "INSERT INTO zone(name) VALUES(%s)"
+            data = (_name)
+            cursor = db.cursor()
+            #cursor.execute(sql, data)
+            #db.commit()
+            resp = jsonify({"status": 200, "message": "Zone added succesfully"})
+            resp.status_code = 200
+            return resp
+        except Exception as e:
+          print(e)
+        finally:
+          cursor.close() 
+          db.close()
+
+    @classmethod
+    def delete(self, _json):
+        db = None
+        cursor = None
+        try:
+          db = mysql.connect()
+          _json = request.json
+          _id = _json['id']
+
+          if _id and request.method == 'POST':
+            sql = "DELETE FROM zone WHERE id=%s"
+            data = (_id)
+            cursor = db.cursor()
+            cursor.execute(sql, data)
+            db.commit()
+            resp = jsonify({"status": 200, "message": "Zone removed succesfully"})
+            resp.status_code = 200
+            return resp
+        except Exception as e:
+          print(e)
+        finally:
+          cursor.close() 
+          db.close()
---
+++ routes/local.py
@@ -0,0 +1,23 @@
+from flask import Blueprint
+from flask import request
+from flask_jwt import jwt_required
+from models.ModelLocal import ModelLocal
+
+local = Blueprint('local', __name__, url_prefix='/local')
+
+@local.get('/')
+@jwt_required()
+def get_all():
+  return ModelLocal.get_all()
+
+@local.post('/add')
+@jwt_required()
+def add():
+  _json = request.json
+  return ModelLocal.add(_json)
+
+@local.post('/delete')
+@jwt_required()
+def delete():
+  _json = request.json
+  return ModelLocal.delete(_json)
