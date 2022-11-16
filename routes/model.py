dd66a3def43e68b6e422470d8add38a7b80336ee
Mau-MR <maudevops@gmail.com>
2022-11-15 18:25:17 -0600

    Inference working

--- routes/model.py
+++ routes/model.py
@@ -1,16 +1,19 @@
 from flask import Blueprint, request, jsonify
+from PIL import Image
 import torch
 import os
+import io
 model = Blueprint('model', __name__)
 
 path = os.path.join(os.getcwd(), 'best.pt')
 print("Taking model from:", path)
-best = torch.hub.load('ultralytics/yolov5', 'custom', path=path, force_reload=True)
-best.eval()
+yolo = torch.hub.load('ultralytics/yolov5', 'custom', path=path, force_reload=True)
 @model.route('/image', methods=['POST'])
 def inference():
     if 'image' not in request.files:
         return jsonify({"done": False, 'message': 'There was no image for inference'})
     image = request.files['image']
-    results = best(image)
-    return jsonify(results)
+    image = Image.open(io.BytesIO(image.read()))
+    results = yolo(image)
+    data = results.pandas().xyxy[0].to_json(orient="records")
+    return data
